503db3abd8c753288a5be279ebb90aa6deb1a856
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-06-15 10:44:08 +0200

    adding close event issue1

---
+++ starterOnly/modal.js
@@ -0,0 +1,37 @@
+function editNav() {
+  const topNav = document.getElementById("myTopnav");
+  if (topNav.className === "topnav") {
+    topNav.className += " responsive";
+  } else {
+    topNav.className = "topnav";
+  }
+}
+
+// DOM Elements
+const modalbg = document.querySelector(".bground");
+const modalBtn = document.querySelectorAll(".modal-btn");
+const formData = document.querySelectorAll(".formData");
+
+const closeBtnForm = document.querySelector(".close");
+
+// launch modal event
+modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+
+// launch modal form
+function launchModal() {
+  modalbg.style.display = "block";
+}
+
+// close modal form issue 1
+const closeModalFunction = () => {
+  modalbg.style.display = "none"
+} 
+
+// closeh modal event issue 1
+closeBtnForm.addEventListener("click", closeModalFunction)
+
+
+
+
+
+
