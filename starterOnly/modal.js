ae395bd93a73dcdf18b63f65de0374d0ed5be6fb
[ALexandre Toullec] <[alexandre.toullec@gmail.com]>
2023-06-28 10:59:24 +0200

    modifying responsive  menu

--- starterOnly/modal.js
+++ starterOnly/modal.js
@@ -1,3 +1,17 @@
+// DOM Elements
+const modalbg = document.querySelector(".modalForm-bg");
+
+//  why using query SelectorAll when you only have one modalButton ???
+const modalBtn = document.querySelector(".modal-btn");
+
+const formData = document.querySelectorAll(".formData");
+
+const closeBtnForm = document.querySelector(".close");
+
+
+
+
+
 // ?????
 
 function editNav() {
@@ -9,15 +23,11 @@ function editNav() {
   }
 }
 
-// DOM Elements
-const modalbg = document.querySelector(".bground");
-const modalBtn = document.querySelectorAll(".modal-btn");
-const formData = document.querySelectorAll(".formData");
 
-const closeBtnForm = document.querySelector(".close");
+
 
 // launch modal event
-modalBtn.forEach((btn) => btn.addEventListener("click", launchModal));
+modalBtn.addEventListener("click", launchModal);
 
 // launch modal form
 function launchModal() {
@@ -26,13 +36,37 @@ function launchModal() {
 
 // close modal form issue 1
 const closeModalFunction = () => {
-  modalbg.style.display = "none"
+  modalbg.style.display = "none";
 } 
 
 // close modal event issue 1
 closeBtnForm.addEventListener("click", closeModalFunction)
 
 
+
+// RESPONSIVE MOBILE MENU
+const menuWrapper = document.querySelector(".nav-wrapper");
+const menu = document.querySelector(".nav-list");
+const hamburger = document.querySelector(".hamburger");
+const close = document.querySelector(".close");
+
+const showMenu = () => {
+  hamburger.style.display = "none";
+  close.style.transform = "translateY(0)";
+  menuWrapper.style.transform = "translateX(0)";
+  menu.style.transform = "translateX(0)";
+};
+
+const hideMenu = () => {
+  close.style.transform = "translateY(-20rem)";
+  hamburger.style.display = "block";
+  menuWrapper.style.transform = "translateX(-200%)";
+  menu.style.transform = "translateX(200%)";
+};
+
+hamburger.addEventListener("click", showMenu);
+close.addEventListener("click", hideMenu);
+menuWrapper.addEventListener("click", hideMenu);
 
 
 
